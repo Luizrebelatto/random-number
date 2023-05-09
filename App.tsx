b505cf00e38fd2399c004af8fae1d7a7b1962be2
Luiz Gabriel <lbrebelatto@gmail.com>
2023-05-09 14:31:05 -0300

    feat: create textInput

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import { View, Text, TextInput } from 'react-native';
 export default function App() {
+
+  const [weight, setWeight] = useState<string>();
+
   return (
     <View>
         <Text>Qual seu Peso?</Text>
+        <TextInput
+           placeholder='Digite seu peso: kg'
+           keyboardType='numbers-and-punctuation'
+           value={weight}
+           onChangeText={(value) => setWeight(value)}
+        />
     </View>
   );
 }
